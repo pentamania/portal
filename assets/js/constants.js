7aa65cc76b471e2a5faed0280bf4f350898e51ac
pentamania <tanigami7220@gmail.com>
2017-02-24 19:05:31 +0900

    Add SIIG Shadow link, etc.

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -108,18 +108,26 @@ var ASSETS = {
   var ZATTA = [
     {
       "name" : "フラワーマスター",
-      "descriptions": "花",
+      "descriptions": "＊花＊",
       "url" : "https://pentamania.github.io/flower-master/",
     },
     {
       "name" : "未完成パズルゲーム",
       "url" : "https://pentamania.github.io/chem-puzzle/build/",
     },
+    // 実行時、enchantの処理がおかしくなるため、保留
     // {
-    //   "name" : "SIIG Shadow",
-    //   "descriptions": "影",
-    //   "url" : "",
+      // "name" : "幸子",
+      // "url" : "javascript:(function(){var c='color:#FFE5FF;background:#b6c2e9;font-size:.6em;vertical-align:middle;',d=document.body.innerHTML,_p='%E3%82%AB%E3%83%AF%E3%82%A4%E3%82%A4%EF%BC%8A',_s='%EF%BC%8A%E3%82%AB%E3%83%AF%E3%82%A4%E3%82%A4',p='<span style='+c+'>'+_p+'</span>',s='<span style='+c+'>'+_s+'</span>',n=d.match(/%E3%81%95%E3%81%A1%E3%81%93|%E3%82%B5%E3%83%81%E3%82%B3|%E3%81%95%E3%81%A1%E5%AD%90|%E5%B9%B8%E5%AD%90|%E8%BC%BF%E6%B0%B4%E5%B9%B8%E5%AD%90|%E3%81%93%E3%81%97%E3%81%BF%E3%81%9A%E3%81%95%E3%81%A1%E3%81%93|%E3%82%B3%E3%82%B7%E3%83%9F%E3%82%BA%E3%82%B5%E3%83%81%E3%82%B3|%E3%81%95%E3%81%A3%E3%81%A1%E3%82%93|%E3%81%95%E3%81%A3%E3%81%A1%E3%82%83%E3%82%93|%E3%81%95%E3%81%A3%E3%81%A1%E3%83%BC/mg),u,i,l;if(n){for(i=0,l=n.length;i<l;i++){u=new RegExp(n[i]+'(?!'+s+')');d=d.replace(u,p+n[i]+s);}document.body.innerHTML=d;}})();",
+      // "url" : 'http://pentamania.hatenablog.com/entry/2015/12/10/201342',
+      // "descriptions": "ブックマークレット",
     // },
+    {
+      "name" : "SIIG Shadow",
+      "descriptions": "† 影 †",
+      "url" : "https://www47.atpages.jp/ptmania/works/shadow",
+    },
+
   ];
   window.ZATTA_LIST = ZATTA;
 
